45d31376f8fa10824cf7e06df5a747288142cf1b
f3st <f3st@M-VWJ7HT9R3H.nordstrom.net>
2024-08-31 18:24:20 -0700

    mongo DB as a data store

--- main.go
+++ main.go
@@ -1,26 +1,51 @@
 package main
 
 import (
+	"context"
 	b64 "encoding/base64"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/patrickmn/go-cache"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var localCache *cache.Cache
 var authorityMap map[string]bool
+var collection *mongo.Collection
+
+const uri = "mongodb://127.0.0.1:27017"
 
 func main() {
 
 	localCache = cache.New(5*time.Second, 10*time.Second)
 	authorityMap = make(map[string]bool)
 	hydrateAuthorityMap(authorityMap)
+	collection = connectToDB().Database("fortknox-db").Collection("tokenStore")
 	handleRequests()
+}
+
+func connectToDB() *mongo.Client {
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	return client
 }
 
 func hydrateAuthorityMap(authorityMap map[string]bool) {
@@ -62,9 +87,17 @@ type RedeemResponse struct {
 	Authority string
 }
 
-func getToken(response http.ResponseWriter, request *http.Request) {
+type CacheRequest struct {
+	Key   string
+	Value string
+}
 
-	// fmt.Printf("authority map %v", authorityMap)
+type MongoDBTokenStoreDocument struct{
+	Sde string `json:"sde"`
+	Token string `json:"token"`
+}
+
+func getToken(response http.ResponseWriter, request *http.Request) {
 
 	var tokenRequest TokenRequest
 	var tokenResponse TokenResponse
@@ -73,7 +106,7 @@ func getToken(response http.ResponseWriter, request *http.Request) {
 
 	if error != nil {
 		response.WriteHeader(http.StatusInternalServerError)
-		log.Fatalf("Somthing is wrong. Please try again later. error: %v sde: %v", error, tokenRequest)
+		log.Fatalf("Something is wrong while decoding the request. Please try again later. error: %v sde: %v", error, tokenRequest)
 	}
 
 	_, ok := authorityMap[tokenRequest.Authority]
@@ -85,12 +118,55 @@ func getToken(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	// fmt.Printf("tokenRequest %+v \n", tokenRequest)
-
 	tokenResponse.Sde = tokenRequest.Sde
-	tokenResponse.Token = tokanizeSDE(tokenRequest.Sde)
-	token := tokanizeSDE(tokenRequest.Sde)
-	localCache.Set(token, tokenRequest.Sde, cache.DefaultExpiration)
+
+	// search in cache
+	token, found := localCache.Get(tokenRequest.Sde)
+
+	// if found, return
+
+	if found {
+		log.Printf("token found in cache")
+		tokenResponse.Token = token.(string)
+	} else {
+
+		// if not found, search in DB
+
+		var result MongoDBTokenStoreDocument
+
+		filter := bson.D{{Key: "sde", Value: tokenRequest.Sde}}
+
+		err := collection.FindOne(context.TODO(), filter).Decode(&result)
+
+		// if found, write to cache and return
+		if err == nil {
+
+			log.Printf("token found in DB")
+
+			tokenResponse.Token = result.Sde
+			localCache.Set(tokenResponse.Token, tokenRequest.Sde, cache.DefaultExpiration)
+			localCache.Set(tokenRequest.Sde, tokenResponse.Token, cache.DefaultExpiration)
+
+		} else {
+
+			// if not found, generate new token, write to DB, write to cache and return
+			tokenResponse.Token = tokanizeSDE(tokenRequest.Sde)
+			localCache.Set(tokenResponse.Token, tokenRequest.Sde, cache.DefaultExpiration)
+			localCache.Set(tokenRequest.Sde, tokenResponse.Token, cache.DefaultExpiration)
+
+			newToken := MongoDBTokenStoreDocument{Sde: tokenRequest.Sde, Token: tokenResponse.Token}
+			_, err := collection.InsertOne(context.TODO(), newToken)
+
+			// if error write to DB then return ERROR
+			if err != nil {
+				log.Fatalf("Something is wrong while inserting to DB. Please try again later. error: %v sde: %v", err, tokenRequest)
+				panic(err)
+			}
+
+			log.Printf("token stored in DB")
+		}
+	}
+
 	tokenResponse.RequestId = getUUID()
 
 	response.WriteHeader(http.StatusOK)
@@ -136,11 +212,11 @@ func redeemToken(response http.ResponseWriter, request *http.Request) {
 
 	// if found in cache, use it
 	if found {
-		fmt.Printf("redeem from cache")
+		log.Printf("redeem from cache")
 		redeemResponse.Sde = sde.(string)
 		// if not then detokenize it using decoder algo
 	} else {
-		fmt.Printf("redeem from decoder")
+		log.Printf("redeem from decoder")
 		redeemSDE := detokanizeSDE(redeemRequest.Token)
 		redeemResponse.Sde = redeemSDE
 	}
